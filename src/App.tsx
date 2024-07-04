c7d72a15b577efc5b36199d8875aca9397d8e154
Nishant <nishant.chorge@futurescapetech.com>
2024-07-04 14:36:17 +0530

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import Preloader from "./components/router/Loader"
 import Router from "./components/router/Routes"
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <SpeedInsights />
       <Router />
+      <Analytics />
     </>
   )
 
